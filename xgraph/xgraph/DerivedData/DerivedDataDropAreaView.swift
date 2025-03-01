03184fe229b38f37e445a20239b4647096ce1911
Anton Timonin <atimonin@ivi.ru>
2025-03-01 18:42:29 +0300

    X Fix drop area

--- xgraph/xgraph/DerivedData/DerivedDataDropAreaView.swift
+++ xgraph/xgraph/DerivedData/DerivedDataDropAreaView.swift
@@ -9,35 +9,42 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct DerivedDataDropAreaView: View {
-    @State private var isTargeted = false
+    
+    // MARK: - Internal properties
+    
     var onDirectoryDropped: (URL) -> Void
-
+    
+    // MARK: - Private properties
+    
+    @State private var isTargeted = false
+    
+    // MARK: - Body
+    
     var body: some View {
-            ZStack {
+        ZStack {
             Rectangle()
-                        .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
-                            .frame(height: 100)
+                .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
+                .frame(height: 100)
                 .overlay(
-                        Text(isTargeted ? "Отпустите DerivedData" : "Перетащите директорию DerivedData сюда")
-                            .foregroundColor(.secondary)
+                    Text(isTargeted ? "Отпустите DerivedData" : "Перетащите директорию DerivedData сюда")
+                        .foregroundColor(.secondary)
                 )
-                .onDrop(of: [.fileURL], isTargeted: $isTargeted) { providers in
-                    handleDrop(providers: providers)
-                }
         }
-        
+        .onDrop(of: [.fileURL], isTargeted: $isTargeted) { providers in
+            handleDrop(providers: providers)
+        }
     }
-
+    
+    // MARK: - Private methods
+    
     private func handleDrop(providers: [NSItemProvider]) -> Bool {
         for provider in providers {
             provider.loadItem(forTypeIdentifier: UTType.fileURL.identifier) { data, _ in
-                if let data = data as? Data,
-                   let url = URL(dataRepresentation: data, relativeTo: nil) {
-                    var isDir: ObjCBool = false
-                    if FileManager.default.fileExists(atPath: url.path, isDirectory: &isDir), isDir.boolValue {
-                        DispatchQueue.main.async {
-                            onDirectoryDropped(url)
-                        }
+                guard  let data = data as? Data, let url = URL(dataRepresentation: data, relativeTo: nil) else { return }
+                var isDir: ObjCBool = false
+                if FileManager.default.fileExists(atPath: url.path, isDirectory: &isDir), isDir.boolValue {
+                    DispatchQueue.main.async {
+                        onDirectoryDropped(url)
                     }
                 }
             }
