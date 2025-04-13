7be3463dd59c9c28f024b9fa5bfde25763f8dd47
Anton Timonin <atimonin@ivi.ru>
2025-04-13 20:19:33 +0300

    X Add main view

---
+++ xgraph/xgraph/View/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  xgraph
+//
+//  Created by Anton Timonin on 13.04.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+
+    var body: some View {
+        VStack{}
+    }
+}
