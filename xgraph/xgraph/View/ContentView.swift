870bd57dacb57d3d7bde8d0f9acebd3f2bee2ed7
Anton Timonin <atimonin@ivi.ru>
2025-04-13 20:41:15 +0300

    X Add drop area for reading project

--- xgraph/xgraph/View/ContentView.swift
+++ xgraph/xgraph/View/ContentView.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
+import Charts
+
+// форматирование даты
+private let dateFormatter: DateFormatter = {
+    let f = DateFormatter()
+    f.dateStyle = .short
+    f.timeStyle = .short
+    return f
+}()
 
 struct MainView: View {
 
+    @StateObject private var derivedDataManager = DerivedDataManager()
+    @State private var selectedLogFile: URL?
+    @State private var activeLog: LogDependencies?
+
     var body: some View {
-        VStack{}
+        VStack {
+            // перетаскивание DerivedData
+            DerivedDataDropAreaView { dir in
+                derivedDataManager.processDerivedData(directory: dir)
+            }
+            .padding()
+            
+            if let graph = derivedDataManager.selectedDependencyGraph {
+                Text("Выбран граф зависимостей с \(graph.keys.count) целями.")
+                    .font(.headline)
+                    .padding()
+            }
+            
+        }
     }
 }
